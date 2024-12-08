fb18526dd303761bf8500da6d06d8a754a9e5ade
enikoandrea123 <enikoandrea@mailbox.unideb.hu>
2024-12-08 10:40:42 +0100

    project init

---
+++ src/main/java/hu/unideb/inf/library_app/LibraryApp.java
@@ -0,0 +1,11 @@
+package hu.unideb.inf.library_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)public class LibraryApp {
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApp.class, args);
+    }
+}
---
+++ src/test/java/hu/unideb/inf/library_app/LibraryAppTest.java
@@ -0,0 +1,11 @@
+package hu.unideb.inf.library_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LibraryAppTest {
+    @Test
+    void contextLoads() {
+    }
+}
