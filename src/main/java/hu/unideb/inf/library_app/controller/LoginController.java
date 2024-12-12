e9145c9a34ff73ebbd882f4fcbdc5b31dbbfcf0f
enikoandrea123 <enikoandrea@mailbox.unideb.hu>
2024-12-12 09:07:06 +0100

    frontend update: login and public pages

--- src/main/java/hu/unideb/inf/library_app/controller/LoginController.java
+++ src/main/java/hu/unideb/inf/library_app/controller/LoginController.java
@@ -1,13 +1,26 @@
 package hu.unideb.inf.library_app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String login() {
-        return "login";  
+    public String loginPage() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String handleLogin(@RequestParam String username, @RequestParam String password, Model model) {
+        if ("admin".equals(username) && "password".equals(password)) {
+            return "redirect:/home";
+        } else {
+            model.addAttribute("error", "Incorrect username or password. Please try again.");
+            return "login";
+        }
     }
 }
